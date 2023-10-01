0ccbfd35a7dc0f1b51cfe9776ce4db784bb429eb
ayeman-07 <140892036+ayeman-07@users.noreply.github.com>
2023-10-01 23:28:07 +0530

    Update RegisterComponent.jsx

--- src/components/RegisterComponent.jsx
+++ src/components/RegisterComponent.jsx
@@ -64,7 +64,7 @@ export default function RegisterComponent() {
       <hr class="hr-text" data-content="or" />
       <div className="google-btn-container">
         <p className="go-to-signup">
-          Already on LinkedIn?{" "}
+          Already on HackBuddy?{" "}
           <span className="join-now" onClick={() => navigate("/")}>
             Sign in
           </span>
